8a415d524b324a661adf449e0353beaf0cedb9be
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-04-10 09:57:45 -0400

    Updating starting files

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+// Selected variables
+var apiKey = "f887ad1638b929c3f93191c21d6e2672"
+var cityName = document.getElementById("cityName").value;
+var date = document.getElementById("currentDate");
+var currentDate = moment().format("M/DD/YYYY");
+console.log(currentDate);
 
+// Display current date in city info
+date.textContent = currentDate;
+
+
+
+
+
+
+
+
+
+
+
+// Icon URL:http://openweathermap.org/img/wn/10d@2x.png
+//5 day forecast: "http://api.openweathermap.org/data/2.5/forecast?q={city name}&appid=f887ad1638b929c3f93191c21d6e2672"
